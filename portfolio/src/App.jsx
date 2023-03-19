b32aa426d05cafeb036eeb916884cb6a1c22f388
Shobhnik13 <shobhnikw@gmail.com>
2023-03-20 01:13:49 +0530

    start

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App bg-red-800">
+      hiiiiii
+    </div>
+  )
+}
+
+export default App
